ba3ef30d6cd511a02e0fc94bafbc7cab1462f5a6
LanceAFate <princelallen1@gmail.com>
2025-01-27 13:02:18 +0800

    commit -m "Added Calculator, DataTypesExample, and Variables classes"

---
+++ Greeting/src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 50;
+        int num2 = 20;
+        double result;
+
+        result = num1 + num2;
+        System.out.println("Addition: " + result);
+
+        result = num1 - num2;
+        System.out.println("Subtraction: " + result);
+
+        result = num1 * num2;
+        System.out.println("Multiplication: " + result);
+
+        result = num1 / num2;
+        System.out.println("Division: " + result);
+    }
+}
---
+++ Greeting/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Prince Lallen";
+        int age = 18;
+        double height = 5.11;
+        boolean isStudent = true;
+
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old. My height is " + height + " and I am a student: " + isStudent);
+    }
+    
+}
